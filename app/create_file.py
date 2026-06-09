8ec2b055ccec1adf5ccdd48ec3ec2c6fd7369dde
ToJaVadym <vadymivaneiko2@gmail.com>
2026-06-09 19:17:28 +0200

    Fixed code

--- app/create_file.py
+++ app/create_file.py
@@ -14,29 +14,34 @@ if "-d" in sys.argv:
 if "-f" in sys.argv:
     f_index = sys.argv.index("-f")
     filename = sys.argv[f_index + 1]
+else:
+    print("No filename provided")
 
 if dirs:
     path = os.path.join(*dirs)
     os.makedirs(path, exist_ok=True)
 
-lines = []
-while True:
-    line = input("Enter content line: ")
-    if line == "stop":
-        break
-    lines.append(line)
+if filename is not None:
 
-if dirs:
-    filepath = os.path.join(*dirs, filename)
-else:
-    filepath = filename
+    lines = []
+    while True:
+        line = input("Enter content line: ")
+        if line == "stop":
+            break
+        lines.append(line)
 
-file_exists = os.path.exists(filepath)
+if filename is not None:
+    if dirs:
+        filepath = os.path.join(*dirs, filename)
+    else:
+        filepath = filename
 
-with open(filepath, "a") as f:
-    if file_exists:
-        f.write("\n")
+    file_exists = os.path.exists(filepath)
 
-    f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
-    for i, line in enumerate(lines, 1):
-        f.write(f"{i} {line}\n")
+    with open(filepath, "a") as f:
+        if file_exists:
+            f.write("\n")
+
+        f.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n")
+        for i, line in enumerate(lines, 1):
+            f.write(f"{i} {line}\n")
